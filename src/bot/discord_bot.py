83dada4d855070454f7d55c7a4d8c4cbc5e882b1
madisonewebb <madisonewebb28@gmail.com>
2026-02-20 13:13:28 -0800

    refactor: move discord bot into src dir

---
+++ src/bot/discord_bot.py
@@ -0,0 +1,25 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+
+token = os.getenv("DISCORD_BOT_TOKEN")
+if not token:
+    print("ERROR: DISCORD_BOT_TOKEN not found in environment variables")
+    print("Make sure your .env file exists and contains: DISCORD_BOT_TOKEN=your_token")
+    exit(1)
+
+client.run(token)
